592521047ab868e2c7be6e616b433492e79b7e3a
Echinfer <echinfer@gmail.com>
2023-09-03 23:42:59 -0400

    decode con cv2

--- ToolsApi/api/views.py
+++ ToolsApi/api/views.py
@@ -16,21 +16,27 @@ class OcrView(View):
     def post(self, request):
         text = ""
         text_correction = ""
+        #img_path1 = r"c:\Users\Echin\Desktop\image-1.jpg"
+        #text = pytesseract.image_to_string(img_path1,lang='spa')
+        #tool = LanguageTool('es')
+        #text_correction = LanguageTool.correct(tool, text)
         if request.method == 'POST' and request.FILES.get('image'):
             # Leer los datos binarios de la imagen
             image_data = request.FILES['image']
-
+            image_np = cv2.imdecode(np.fromstring(image_data.read(), np.uint8), cv2.IMREAD_COLOR)
+            #image_np = Image.open(image_data)
             # Convertir los datos binarios a una matriz NumPy
-            image = cv2.imdecode(np.fromstring(image_data.read(), np.uint8), cv2.IMREAD_COLOR)
+            #image = cv2.imdecode(np.fromstring(image_data.read(), np.uint8), cv2.IMREAD_COLOR)
 
             # Convertir a escala de grises
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            #gray = cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY)
             
             # Aplicar umbralización
-            _, thresholded = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+            #_, image_np = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
             
             # Realiza OCR en la imagen usando Tesseract a través de pytesseract            
-            text = pytesseract.image_to_string(thresholded, lang='spa')
+            #text = pytesseract.image_to_string(thresholded, lang='spa')
+            text = pytesseract.image_to_string(image_np, lang='spa')
 
             tool = LanguageTool('es')
             #correcciones = tool.check(text)
